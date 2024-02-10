a1331e89ffc8eee643833d9e912629c28ac24625
Akumu2022 <114282034+Akumu2022@users.noreply.github.com>
2024-02-10 23:18:12 +0300

    created a sign up api

--- main.py
+++ main.py
@@ -17,6 +17,10 @@ class UserBase(BaseModel):
     password:str
     confirm_password: str
     
+class UserLogin(BaseModel):
+    email: str
+    password: str
+    
 def get_db():
     db=SessionLocal()
     try:
@@ -40,10 +44,17 @@ async def create_user(user:UserBase, db:db_dependency):
     return {"msg":"new user created successfully"}
     
     
-@app.get("/api/v1/users{user_id}",status_code=status.HTTP_200_OK)
+@app.get("/api/v1/users/{user_id}",status_code=status.HTTP_200_OK)
 async def get_user(user_id: str, db:db_dependency):
     user = db.query(models.User).filter(models.User.username == user_id).first()
     if user is None:
         raise HTTPException(status_code=404, detail="No such user")
     return user
 
+
+@app.post("/api/v1/login",status_code=status.HTTP_200_OK)
+async def login_user(user: UserLogin, db: db_dependency):
+    user_from_db = db.query(models.User).filter(models.User.email == user.email).first()
+    if user_from_db is None or user_from_db.password != user.password:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Invalid email or password")
+    return {"msg":"Login successful"}
